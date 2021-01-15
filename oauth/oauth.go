2a1a91078ff2ba5b6d51ddd23446fadfbb39fb25
Andrey Tabakov <komdosh@yandex.ru>
2021-01-15 09:03:25 +0300

    feat(oauth): add shared oauth client for go-bookstore microservices app

---
+++ oauth/oauth.go
@@ -0,0 +1,106 @@
+package oauth
+
+import (
+	"fmt"
+	"github.com/Komdosh/go-bookstore-oauth-client/oauth/rest_error"
+	"github.com/go-resty/resty"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+const (
+	headerXPublic   = "X-Public"
+	headerXClientId = "X-Client-Id"
+	headerXCallerId = "X-Caller-Id"
+
+	paramAccessToken = "access_token"
+
+	oauthBaseURL = "http://127.0.0.1:8080"
+)
+
+var (
+	usersRestClient = resty.New()
+)
+
+type accessToken struct {
+	Id       string `json:"id"`
+	UserId   int64  `json:"user_id"`
+	ClientId int64  `json:"client_id"`
+}
+
+func IsPublic(request *http.Request) bool {
+	if request == nil {
+		return true
+	}
+
+	return request.Header.Get(headerXPublic) == "true"
+}
+
+func GetCallerId(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	callerId, err := strconv.ParseInt(request.Header.Get(headerXCallerId), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return callerId
+}
+
+func GetClientId(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	clientId, err := strconv.ParseInt(request.Header.Get(headerXClientId), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return clientId
+}
+
+func AuthenticateRequest(request *http.Request) *rest_error.RestErr {
+	if request == nil {
+		return nil
+	}
+
+	cleanRequest(request)
+
+	accessTokenId := strings.TrimSpace(request.URL.Query().Get(paramAccessToken))
+	if accessTokenId == "" {
+		return nil
+	}
+
+	at, err := getAccessToken(accessTokenId)
+	if err != nil {
+		if err.Status == http.StatusNotFound {
+			return nil
+		}
+		return err
+	}
+	request.Header.Add(headerXClientId, fmt.Sprintf("%v", at.ClientId))
+	request.Header.Add(headerXCallerId, fmt.Sprintf("%v", at.UserId))
+	return nil
+}
+
+func cleanRequest(request *http.Request) {
+	if request == nil {
+		return
+	}
+	request.Header.Del(headerXClientId)
+	request.Header.Del(headerXCallerId)
+}
+
+func getAccessToken(accessTokenId string) (*accessToken, *rest_error.RestErr) {
+	var restErr *rest_error.RestErr
+	var at accessToken
+	_, err := usersRestClient.R().SetError(&restErr).SetResult(&at).Get(fmt.Sprintf("%s/oauth/access_token/%s", oauthBaseURL, accessTokenId))
+	if restErr != nil {
+		return nil, restErr
+	}
+	if err != nil {
+		return nil, rest_error.NewInternalServerError("error while login user")
+	}
+
+	return &at, nil
+}
